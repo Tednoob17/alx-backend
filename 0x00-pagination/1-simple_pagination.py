b229f33545aaf37d4d587f45679694e07333bc1d
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 12:56:23 +0100

    csv reader

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -13,7 +13,8 @@ def index_range(page: int, page_size: int) -> Tuple:
 
 
 class Server:
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -23,9 +24,7 @@ class Server:
         """Cached dataset
         """
         if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
+            
             self.__dataset = dataset[1:]
 
         return self.__dataset
