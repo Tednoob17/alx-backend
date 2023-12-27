87445d60bc040ce9dcad1f59109b1a77d976de9c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:01:12 +0100

    2- paginate data base

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -12,9 +12,7 @@ def index_range(page: int, page_size: int) -> Tuple:
     return ((page - 1) * page_size, page * page_size)
 
 
-class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
