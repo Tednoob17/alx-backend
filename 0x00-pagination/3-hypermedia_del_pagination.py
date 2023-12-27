46a546a33edf34766fa5cdffba74ef61a2882d34
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:12:01 +0100

    3- baby names

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -10,7 +10,7 @@ from typing import List, Dict
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
