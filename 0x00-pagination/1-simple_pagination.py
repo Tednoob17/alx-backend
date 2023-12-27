d7b9abe91ed0f0e3a4e74ef2320d3491de4bf293
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 12:50:46 +0100

    baby names

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -15,7 +15,7 @@ def index_range(page: int, page_size: int) -> Tuple:
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
