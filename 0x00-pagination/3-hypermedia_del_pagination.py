8e5ccadd5cd9377916d3449d6ff0938734bc6e21
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:17:53 +0100

    :1000

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -31,7 +31,8 @@ class Server:
         """Dataset indexed by sorting position, starting at 0
         """
         if self.__indexed_dataset is None:
-          
+            dataset = self.dataset()
+            truncated_dataset = dataset[:1000]
             self.__indexed_dataset = {
                 i: dataset[i] for i in range(len(dataset))
             }
