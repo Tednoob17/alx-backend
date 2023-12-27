5abeca36a0ec28d0cca16c52d722efc9fdc7f372
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:13:00 +0100

    3- dataset none

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -13,7 +13,7 @@ class Server:
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-
+        self.__dataset = None
         self.__indexed_dataset = None
 
     def dataset(self) -> List[List]:
