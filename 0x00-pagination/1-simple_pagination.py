3b08158608d8baebe573c9b521d4392530a8d1f8
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 12:55:58 +0100

    server class b

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,13 +8,12 @@ from typing import Tuple, List
 
 
 def index_range(page: int, page_size: int) -> Tuple:
-    "
+    """Return a tuple of size two containing a start index and an end index"""
     return ((page - 1) * page_size, page * page_size)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
