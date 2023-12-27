419cce067931200b6d31461f1b44a14a750c7d1e
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:02:59 +0100

    2- added data set

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -20,6 +20,16 @@ class Server:
     def __init__(self):
         self.__dataset = None
 
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return list of rows"""
