21e14f21be8d377aab3e0944d15887303ad2bbd6
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 12:52:07 +0100

    get page

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -31,7 +31,7 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+  
         """Return list of rows"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
