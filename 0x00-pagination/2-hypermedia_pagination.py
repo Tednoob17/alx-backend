8c184d7384e3013063a49da508ab08d158eb0ed9
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:02:03 +0100

    2- cched data set

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -15,21 +15,11 @@ def index_range(page: int, page_size: int) -> Tuple:
 class Server:
     """Server class to paginate a database of popular baby names.
     """
- 
+    DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
-        if self.__dataset is None:
-            with open(self.DATA_FILE) as f:
-                reader = csv.reader(f)
-                dataset = [row for row in reader]
-            self.__dataset = dataset[1:]
-
-        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return list of rows"""
