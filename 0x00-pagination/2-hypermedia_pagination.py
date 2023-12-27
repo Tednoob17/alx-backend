dd3cb000298014bfeeb7fcc44b26ba8c90ef7309
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:03:49 +0100

    2- page_size

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -34,7 +34,8 @@ class Server:
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """Return list of rows"""
         assert isinstance(page, int) and isinstance(page_size, int)
-        
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
         return self.dataset()[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
