3bd816b420ee8a55a22c15cdde11e5fddc923325
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 12:53:28 +0100

    end dataset

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -35,5 +35,5 @@ class Server:
         """Return list of rows"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
         
-        return self.dataset()[start:end]
