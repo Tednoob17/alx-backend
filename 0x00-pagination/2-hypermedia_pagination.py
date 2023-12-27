190ba7e59033eddcf2f60278b40ce71104a3728c
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 13:04:05 +0100

    2- get_hyper

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -38,14 +38,4 @@ class Server:
         start, end = index_range(page, page_size)
         return self.dataset()[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
-        """Return a dictionary"""
-        total_pages = math.ceil(len(self.dataset()) / page_size)
-        return {
-            'page_size': len(self.get_page(page, page_size)),
-            'page': page,
-            'data': self.get_page(page, page_size),
-            'next_page': page + 1 if page < total_pages else None,
-            'prev_page': page - 1 if page > 1 else None,
-            'total_pages': total_pages
-        }
+   
