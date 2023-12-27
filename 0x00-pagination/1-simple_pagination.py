b8941d91b79b9373768177f706a6fb2c3a71dc71
Ted Kouhouenou <teddams047@gmail.com>
2023-12-27 01:39:13 +0100

    data set

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -17,10 +17,10 @@ class Server:
     """
     DATA_FILE = "Popular_Baby_Names.csv"
 
+    def __init__(self):
+        self.__dataset = None
 
-    def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+  
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
