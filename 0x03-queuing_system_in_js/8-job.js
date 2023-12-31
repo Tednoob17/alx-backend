a458171181942a8ed01bd02459f555f02474a57a
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:39:41 +0100

    8 job notif err

--- 0x03-queuing_system_in_js/8-job.js
+++ 0x03-queuing_system_in_js/8-job.js
@@ -5,5 +5,17 @@ function createPushNotificationJobs(jobs, queue) {
     throw Error('Jobs is not an array');
   }
 
+  jobs.forEach((job) => {
+    const pushNotificationJob = queue.create('push_notification_code_3', job);
+    
+    pushNotificationJob.save((err) => {
+      if (!err) console.log(`Notification job created: ${pushNotificationJob.id}`);
+    });
+
+    pushNotificationJob.on('progress', (progress) => {
+      console.log(`Notification job ${pushNotificationJob.id} ${progress}% complete`);
+    });
+  });
+}
 
 module.exports = createPushNotificationJobs;
