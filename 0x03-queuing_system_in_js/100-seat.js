d45a4d6e4c284e1c445ef5d77d16dec07570982d
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:09:39 +0100

    available set

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -15,4 +15,7 @@ client.on('error', (error) => {
     console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
-client.on('connect', 
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
