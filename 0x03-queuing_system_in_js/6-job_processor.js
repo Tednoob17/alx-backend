e4d3227043ee390bdab5c49f5899b7eb7113506d
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:25:22 +0100

    sending notif

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -4,3 +4,7 @@ const kue = require('kue');
 
 const queue = kue.createQueue();
 
+function sendNotification(phoneNumber, message) {
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+}
+
