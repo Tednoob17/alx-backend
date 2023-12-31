72fd81ea869ab2ece33d3211939faa1116733b3f
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:24:19 +0100

    5 fin

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -12,4 +12,11 @@ const job = queue.create('push_notification_code', jobData).save((err) => {
   if (!err) console.log(`Notification job created: ${job.id}`);
 });
 
+job.on('complete', () => {
+  console.log('Notification job completed');
+});
+
+job.on('failed', () => {
+  console.log('Notification job failed');
+});
 
