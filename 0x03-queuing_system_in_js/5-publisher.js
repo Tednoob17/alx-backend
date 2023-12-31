f5306994e6a948625c353c79ab81723d91e0e1f3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:16:47 +0100

    5 KILL SERVER

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -16,4 +16,12 @@ function publishMessage(message, time) {
   setTimeout(() => {
     console.log(`About to send ${message}`);
     client.publish('holberton school channel', message);
-  }, tim
+  }, time);
+}
+
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);
+
+module.exports = client;
