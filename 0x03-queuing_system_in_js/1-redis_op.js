8297d248f2fd09aa6e429ebf6aa5da2b08232d94
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:11:59 +0100

    Holberto

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -8,3 +8,18 @@ client.on('error', (error) => {
   console.log(`Redis client not connected to the server: ${error.message}`);
 });
 
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+function displaySchoolValue(schoolName) {
+  client.get(schoolName, (_, res) => {
+    console.log(res);
+  });
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+
+module.exports = client;
