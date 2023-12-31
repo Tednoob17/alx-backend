668e5209b524a0b54888cb8e72c548dd93ec94e3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:47:30 +0100

    9 message

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -4,3 +4,10 @@ import express from 'express';
 import { createClient } from 'redis';
 import { promisify } from 'util';
 
+const app = express();
+const client = createClient();
+
+client.on('error', (error) => {
+    console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
