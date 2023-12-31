229896d124503b6adaad2ff3777315902e9544c2
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:27:15 +0100

    jobs static code

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -3,3 +3,16 @@
 const kue = require('kue');
 const queue = kue.createQueue();
 
+const jobs = [
+  pushNotificationJob.on('complete', () => {
+    console.log(`Notification job ${pushNotificationJob.id} completed`);
+  });
+
+  pushNotificationJob.on('failed', (errorMessage) => {
+    console.log(`Notification job ${pushNotificationJob.id} failed: ${errorMessage}`);
+  });
+
+  pushNotificationJob.on('progress', (progress) => {
+    console.log(`Notification job ${pushNotificationJob.id} ${progress}% complete`);
+  });
+});
