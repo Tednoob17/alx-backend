616e2623a4ab3168794215a5e31f22cf32bccd78
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:15:41 +0100

    4 redis end

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -12,6 +12,12 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
+client.hset('HolbertonSchools', 'Portland', '50', redis.print);
+client.hset('HolbertonSchools', 'Seattle', '80', redis.print);
+client.hset('HolbertonSchools', 'New York', '20', redis.print);
+client.hset('HolbertonSchools', 'Bogota', '20', redis.print);
+client.hset('HolbertonSchools', 'Cali', '40', redis.print);
+client.hset('HolbertonSchools', 'Paris', '2', redis.print);
 
 client.hgetall('HolbertonSchools', (_, value) => {
   console.log(value);
