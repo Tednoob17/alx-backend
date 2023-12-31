5791f3b4a39a40bb0d0c908ee387647de5e49875
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:10:00 +0100

    key processing

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -19,3 +19,45 @@ client.on('connect', () => {
     console.log('Redis client connected to the server');
 });
 
+const reserveSeat = async (number) => {
+    promisify(client.set).bind(client)('available_seats', number);
+};
+
+const getCurrentAvailableSeats = async () => {
+    return promisify(client.get).bind(client)('available_seats');
+};
+
+app.get('/available_seats', async (_, res) => {
+    const availableSeats = await getCurrentAvailableSeats();
+    res.json({ numberOfAvailableSeats: availableSeats });
+});
+
+app.get('/reserve_seat', async (_, res) => {
+    if (!reservationEnabled) {
+        return res.json({ status: 'Reservation are blocked' });
+    }
+    const availableSeats = await getCurrentAvailableSeats();
+    if (availableSeats <= 0) {
+        return res.json({ status: 'Reservation are blocked' });
+    }
+    const job = queue.create('reserve_seat', {}).save((error) => {
+        if (!error) {
+            res.json({ status: 'Reservation in process' });
+        } else {
+            res.json({ status: 'Reservation failed' });
+        }
+    });
+    job.on('complete', () => {
+        console.log(`Seat reservation job ${job.id} completed`);
+    });
+    job.on('failed', (error) => {
+        console.log(`Seat reservation job ${job.id} failed: ${error}`);
+    });
+});
+
+
+app.listen(1245, () => {
+    reserveSeat(50);
+    reservationEnabled = true;
+    console.log('API available on localhost port 1245');
+});
