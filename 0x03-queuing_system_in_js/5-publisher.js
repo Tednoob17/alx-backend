df958d95204a9c5f48724fd2e549fe937ff741c2
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:16:13 +0100

    5 publisher redis error

--- 0x03-queuing_system_in_js/5-publisher.js
+++ 0x03-queuing_system_in_js/5-publisher.js
@@ -8,9 +8,6 @@ client.on('connect', () => {
   console.log('Redis client connected to the server');
 });
 
-client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error.message}`);
-});
 
 function publishMessage(message, time) {
   setTimeout(() => {
