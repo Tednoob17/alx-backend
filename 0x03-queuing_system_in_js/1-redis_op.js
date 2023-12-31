df62f6361ff4c6dfd49ee1fd6c5567b534f004ad
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:11:44 +0100

    back error and sch name

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -4,3 +4,7 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
