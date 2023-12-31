e736deb19c552a497251fb61fae9515d6d7c75b3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:37:55 +0100

    notice phone num

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -7,4 +7,11 @@ const blacklistedPhoneNumbers = [
   '4153518780',
   '4153518781',
 ];
- 
+
+
+queue.process('push_notification_code_2', (job, done) => {
+  const { phoneNumber, message } = job.data;
+
+  sendNotification(phoneNumber, message, job, done);
+  done();
+});
