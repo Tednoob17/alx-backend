869aae57ded9246ed4cf8e5f62d4bf70baf11dc6
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:13:23 +0100

    2 red error cli

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -5,3 +5,9 @@
 import redis from 'redis';
 import { promisify } from 'util';
 
+const client = redis.createClient();
+
+client.on('error', (error) => {
+  console.log(`Redis client not connected to the server: ${error.message}`);
+});
+
