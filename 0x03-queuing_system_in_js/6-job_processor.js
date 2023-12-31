d9a87c634e1d65d541526e5dc11466137f31cae3
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:25:39 +0100

    phone number

--- 0x03-queuing_system_in_js/6-job_processor.js
+++ 0x03-queuing_system_in_js/6-job_processor.js
@@ -8,3 +8,6 @@ function sendNotification(phoneNumber, message) {
   console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
 }
 
+queue.process('push_notification_code', (job, done) => {
+  const { phoneNumber, message } = job.data;
+
