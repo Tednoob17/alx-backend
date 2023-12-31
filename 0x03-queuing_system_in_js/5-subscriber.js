76bd33f47243b3ccd74c78dd5bcc012dad581e48
Ted Kouhouenou <teddams047@gmail.com>
2023-12-31 13:19:12 +0100

    5 subscriber dec

--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -3,25 +3,5 @@ import redis from 'redis';
 
 const client = redis.createClient();
 
-client.on('error', (error) => {
-  console.log(`Redis client not connected to the server: ${error.message}`);
-});
-
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
-
-client.subscribe('holberton school channel');
-
-client.on('message', (channel, message) => {
-  if (channel === 'holberton school channel') {
-    console.log(message);
-  }
-
-  if (message === 'KILL_SERVER') {
-    client.unsubscribe();
-    client.quit();
-  }
-});
 
 module.exports = client;
